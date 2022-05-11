5e6d48325c4763a4469d932a6b5585e358746fb8
sloww <vinicius.slovinski@hotmail.com>
2022-05-11 18:16:48 -0300

    1st commit

---
+++ PWM_lab/PWM_lab.ino
@@ -0,0 +1,28 @@
+#include <TimerOne.h>
+
+#define PINO_PWM 9           
+
+
+unsigned int  Fs = 331,          //setar frequencia em Hertz
+              dutyCycle = 50;   //setar dutyCycle entre 0 e 100%
+        
+
+void setup() {
+  
+  Serial.begin(115200); 
+  
+  long int periodo  = (1000000/Fs);
+  unsigned int duty = (dutyCycle*1024)/100;
+
+
+  pinMode(PINO_PWM, OUTPUT);
+  
+  Timer1.initialize(periodo);
+  Timer1.pwm(PINO_PWM, duty, periodo);  
+
+}
+
+
+void loop() {
+
+}
